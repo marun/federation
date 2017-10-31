f8929b855ecccde88d0e66bcec00d62376c401fb
Maru Newby <marun@redhat.com>
2017-10-30 23:28:05 -0700

    fixup prune deps

--- hack/prune-deps.sh
+++ hack/prune-deps.sh
@@ -21,30 +21,35 @@ set -e
 # The staging areas will be vendored as repos
 rm -rf vendor/k8s.io/kubernetes/staging
 
-glide-vc
+glide-vc --use-lock-file
 
 # we shouldn't have modified anything
 git diff-index --name-only --diff-filter=M HEAD | xargs -r git checkout -f
 # we need to preserve code that is not referenced in the code
 git diff-index --name-only HEAD | grep -F \
+  -e 'BUILD' \
+  -e 'LICENSE' \
   -e 'github.com/jteeuwen/go-bindata' \
   -e 'github.com/onsi/ginkgo/ginkgo' \
+  -e 'k8s.io/.*/fake' \
+  -e 'k8s.io/apiserver/pkg/storage/etcd/testing' \
   -e 'k8s.io/gengo' \
   -e 'k8s.io/kube-openapi' \
   -e 'k8s.io/kubernetes/cluster' \
   -e 'k8s.io/kubernetes/cmd/gen' \
   -e 'k8s.io/kubernetes/examples' \
   -e 'k8s.io/kubernetes/hack' \
+  -e 'k8s.io/kubernetes/pkg/api/testing' \
+  -e 'k8s.io/kubernetes/pkg/kubectl/cmd/testing' \
+  -e 'k8s.io/kubernetes/pkg/registry/registrytest' \
   -e 'k8s.io/kubernetes/pkg/util/template' \
   -e 'k8s.io/kubernetes/test/e2e/testing-manifests' \
   -e 'k8s.io/kubernetes/test/fixtures' \
   -e 'k8s.io/kubernetes/test/images' \
   -e 'k8s.io/kubernetes/translations' \
   -e 'vendor/k8s.io/apimachinery/pkg/util/sets/types' \
-  -e 'vendor/k8s.io/code-generator' \
   -e 'vendor/k8s.io/client-go/util/cert/testdata' \
-  -e 'LICENSE' \
-  -e 'BUILD' \
+  -e 'vendor/k8s.io/code-generator' \
   | grep -v 'vendor/github.com/jteeuwen/go-bindata/testdata' \
   | grep -v 'vendor/k8s.io/kubernetes/staging' \
   | xargs -r git checkout -f
